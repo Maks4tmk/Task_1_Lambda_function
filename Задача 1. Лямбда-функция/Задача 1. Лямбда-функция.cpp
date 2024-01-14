63401bd1e2d8360175875006b2f6b2e25cfb5093
Maksim <maks4tmk5423@yandex.ru>
2024-01-14 20:40:20 +0300

    initial_commit

---
+++ Задача 1. Лямбда-функция/Задача 1. Лямбда-функция.cpp
@@ -0,0 +1,32 @@
+﻿// Задача 1. Лямбда-функция.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	std::vector<int> arr{ 4,7,9,14,12 };
+
+	std::cout << "Входные данные: ";
+	for (int num : arr) {
+		std::cout << num << " ";
+	}
+	std::cout << "\n";
+
+	std::for_each(arr.begin(), arr.end(), [](int& num) {
+		if (num % 2 > 0) {num *= 3;}
+	});
+
+	std::cout << "Выходные данные: ";
+	for (int num : arr) {
+		std::cout << num << " ";
+	}
+
+
+
+}
+
